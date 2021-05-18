a7dc47a0cf94960133c67fe688699382fec118fe
Mateus Machado <mateusmmachado2@gmail.com>
2021-05-18 00:24:06 -0300

    first commit

---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TesteMigrations.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+    }
+
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(u => u.Nome).NotEmpty().WithMessage("Informe um nome!").Length(1, 60);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,73 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TesteMigrations.Data;
+using TesteMigrations.Models;
+
+namespace TesteMigrations
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews().AddFluentValidation();
+
+            services.AddTransient<IValidator<User>, UserValidator>();
+
+            var connection = Configuration.GetConnectionString("TesteDbContext");
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 22));
+
+            services.AddDbContext<TesteDbContext>(
+                dbContextOptions => dbContextOptions.UseMySql(connection, serverVersion)
+                .EnableSensitiveDataLogging() //Remove for production
+                .EnableDetailedErrors() //Remove for production
+                );
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
